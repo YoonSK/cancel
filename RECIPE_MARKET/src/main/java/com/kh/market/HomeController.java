7e152c1faeba19c28cd48d1648e61163fb384ea1
YSG <YSG@DESKTOP-TCC74HV>
2020-04-06 12:29:32 +0900

    test3

--- RECIPE_MARKET/src/main/java/com/kh/market/HomeController.java
+++ RECIPE_MARKET/src/main/java/com/kh/market/HomeController.java
@@ -1,7 +1,5 @@
 package com.kh.market;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -22,7 +20,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "home.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
